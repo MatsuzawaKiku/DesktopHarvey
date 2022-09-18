05331e342de67849688c716fe454e6197a610af8
MatsuzawaKiku <yixuan.qin@mail.mcgill.ca>
2022-09-18 14:46:20 -0400

    enable mouse drag

--- MyFrame.java
+++ MyFrame.java
@@ -3,38 +3,61 @@
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.random.RandomGenerator;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 
-public class MyFrame{
-    static JFrame frame;
-    //Character harvey;
-    static JPanel panel;
+public class MyFrame extends JFrame {
     static JLabel label;
-    Random random = new Random();
+    static Random random;
+    static JPanel panel;
+    int x,y;
 
     /*
     constructor
      */
     public MyFrame(){
-        frame = new JFrame();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setSize(screenSize);
+        this.setSize(screenSize);
+
 
         panel = new JPanel();
+        //panel layout = null can custom component location
+        panel.setLayout(null);
+        panel.setOpaque(false);
+        this.add(panel);
+
         label = new JLabel(); //JLabel Creation
         label.setIcon(new ImageIcon("harvey.png")); //Sets the image to be displayed as an icon
         Dimension size = label.getPreferredSize(); //Gets the size of the image
-        label.setBounds(50, 30, size.width, size.height); //Sets the location of the image
+        label.setBounds(0, 0, size.width, size.height); //Sets the location of the image
         panel.add(label);
-        frame.add(panel);
-        frame.setVisible(true);
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                x = e.getX();
+                y = e.getY();
+            }
+        });
+
+        label.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                JLabel j1 = (JLabel) e.getSource();
+                j1.setLocation(j1.getX() + e.getX() - x, j1.getY() + e.getY()-y);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+        this.setUndecorated(true);
+        this.setBackground(new Color(0,0,0,0));
+        this.setVisible(true);
     }
 
     //this main method opens a frame
@@ -42,48 +65,14 @@ public class MyFrame{
 
         MyFrame myFrame = new MyFrame();
 
-        panel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                super.mouseDragged(e);
-                //if (e.getX() >= label.getX() + (label.getWidth() / 2)){
 
-                //if (label != null){
-                    label.setLocation(e.getX() - label.getWidth()/2 ,e.getY() - label.getHeight()/2);
-                //}
-//                frame.add(panel);
-//                frame.setVisible(true);
-
-            }
-        });
-        panel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseMoved(MouseEvent e) {
-                super.mouseMoved(e);
-                label.setLocation(e.getX(),e.getY());
-                //frame.add(panel);
-                //frame.setVisible(true);
-            }
-        });
-        panel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                JLabel message = new JLabel(message());
-                panel.setLayout(new FlowLayout());
-                panel.add(message);
-                frame.add(panel);
-                frame.setVisible(true);
-
-            }
-        });
 
 
     }
     private static String message(){
         ArrayList<String> database = new ArrayList(Arrays.asList("噢！嗨，菊酱。你……要问诊吗？","不要过度劳累，菊酱。这是医嘱！你的健康对我来说很重要。","我很愿意进一步了解你，菊酱。我们先把医患关系抛到一边。"
         ,"你脸色很不错。很好"));
-        Random random = new Random();
+        random = new Random();
         int loveMes = random.nextInt(4);
         return database.get(loveMes);
     }
